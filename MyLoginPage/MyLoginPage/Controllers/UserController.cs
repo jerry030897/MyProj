d35d2523e504e7655736af50a847f497853f3391
jerry030897 <jerry030897@gmail.com>
2017-07-14 15:41:25 +0800

    登入功能完成(無驗證)

---
+++ MyLoginPage/MyLoginPage/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using MyLoginPage.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyLoginPage.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel pageData)
+        {
+            if ( !string.IsNullOrWhiteSpace(pageData.Account) && !string.IsNullOrWhiteSpace(pageData.Password))
+            {
+                pageData.Message =
+                $"您使用{pageData.Account}登入成功。";
+            }
+            return View(pageData);
+        }
+    }
+}
